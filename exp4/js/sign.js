a84fd39aa5f77d40b4e9efe51ea37b4e43baf9f1
Janvi Karia <47447050+janvi2001@users.noreply.github.com>
2020-09-03 02:50:52 +0530

    Update sign.js

--- exp4/js/sign.js
+++ exp4/js/sign.js
@@ -1,13 +1,13 @@
 $(function(){
-    $('#inputname').on('keypress',function(){
+    $('#inputname').on('keypress keydown keyup',function(){
         if(/[^a-zA-Z ]/.test(this.value)) {
             alert("Invalid Username.Alphabets and spaces only");
             this.value="";
             $(this).focus();
-            return false;
+            
         }
     });
-    $('inputmail').on('blur',function(){
+    $('inputEmail').on('blur',function(){
         if(/[^0-9A-Za-z.@_]/.test(this.value)){
             alert("Invalid email address");
             this.value="";
@@ -22,5 +22,15 @@ $(function(){
             $(this).focus();
         }
     });
+    $('#inputusername').on('keypress keydown keyup',function(){
+        if(/[^a-zA-Z ]/.test(this.value)) {
+            alert("Invalid Username.Alphabets and spaces only");
+            this.value="";
+            $(this).focus();
+            
+        }
+    });
+    
 });
+
 
